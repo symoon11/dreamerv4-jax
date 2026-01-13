12c8729f58cf90d5057fd33585cd199e89846cc5
symoon11 <symoon11@snu.ac.kr>
2026-01-13 20:39:52 +0900

    update dreamerv4

--- src/dreamerv4_jax/models/dreamerv4.py
+++ src/dreamerv4_jax/models/dreamerv4.py
@@ -2,6 +2,8 @@ import jax
 import jax.numpy as jnp
 from flax import nnx
 
+from sgl_jax.srt.layers.linear import LinearBase
+
 
 class DreamerV4MLP(nnx.Module):
     def __init__(
@@ -13,3 +15,44 @@ class DreamerV4MLP(nnx.Module):
         dtype: jnp.dtype = jnp.float16,
     ):
         self.layer_id = layer_id
+
+        self.gate_proj = LinearBase(
+            input_size=hidden_size,
+            output_size=intermediate_size,
+            kernel_axes=(None, "tensor"),
+            use_bias=False,
+            params_dtype=dtype,
+            mesh=mesh,
+        )
+
+        self.up_proj = LinearBase(
+            input_size=hidden_size,
+            output_size=intermediate_size,
+            kernel_axes=(None, "tensor"),
+            use_bias=False,
+            params_dtype=dtype,
+            mesh=mesh,
+        )
+
+        self.down_proj = LinearBase(
+            input_size=intermediate_size,
+            output_size=hidden_size,
+            kernel_axes=("tensor", None),
+            use_bias=False,
+            params_dtype=dtype,
+            mesh=mesh,
+        )
+
+        self.act_fn = jax.nn.silu
+
+    def __call__(self, hidden_states: jax.Array) -> jax.Array:
+        a1, _ = self.gate_proj(hidden_states)
+        a2, _ = self.up_proj(hidden_states)
+        intermediate_parallel = a2 * self.act_fn(a1)
+        output, _ = self.down_proj(intermediate_parallel)
+        return output
+
+
+class DreamerV4Attention(nnx.Module):
+    def __init__(self, hidden_size: int, num_heads: int, num_kv_heads: int, layer_id: int = 0):
+        pass
